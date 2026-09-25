7992cce10ecfe40e67c37387debf5b8c8fa76428
Obaga Joshua <obaga.joshua@student.moringaschool.com>
2026-09-25 11:12:00 +0300

    js lab

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
 
+function calculateTax(amount) {
+  return amount * 0.1;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word) {
+  const cleaned = word.toLowerCase();
+  const reversed = cleaned.split("").reverse().join("");
+  return cleaned == reversed;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - originalPrice * (discountPercentage / 100);
+}
